9aa635f5a8e915a4059f87a39023ed8bf3fb810d
Vaibhav Khadilkar <vaibhav.v.khadilkar@gmail.com>
2012-06-11 13:52:03 -0500

    Added Query and Analyze topologies

--- src/examples/java/edu/utdallas/cs/stormrider/examples/twitter/TwitterTopologies.java
+++ src/examples/java/edu/utdallas/cs/stormrider/examples/twitter/TwitterTopologies.java
@@ -25,8 +25,96 @@ public class TwitterTopologies
 {
 	public void main( String[] args )
 	{
-		//Create and Submit Add Topology
+		//Create a topology object
 		Topology topology = TopologyFactory.getStormTopology() ;
+		
+		//Create and Submit Add Topology
 		topology.submitAddTopology( TwitterConstants.STORAGE_TOPOLOGY_NAME, false, TwitterConstants.NUM_OF_WORKERS, TwitterLoaderTopology.constructAddTopology() ) ;
+		
+		//Submit various queries to the query topology
+		//A simple query that tracks users in a particular location
+		String queryString =
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" SELECT ?x " +
+		" WHERE { ?x twitter:Location \"Richardson, TX\" } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+				              TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+				              TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-simple1-location" ) ;
+		
+		//A query that tracks the neighborhood of a user
+		queryString = 
+		" PREFIX ex: <http://www.example.org/people#> " +
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" SELECT ?x " +
+		" WHERE { <ex:u-15000001> gleen:onPath(\"([twitter:Has_Friend]/[twitter:Has_Friend])\" ?x) } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+	              			  TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+	              			  TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-complex1-neighborhood" ) ;
+
+		//A user that queries an older version of the graph
+		queryString = 
+		" PREFIX ex: <http://www.example.org/people#> " +
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+		" SELECT ?z " +
+		" WHERE " +
+		" { " +
+		"		?x twitter:Timestamp ?y . " +
+		"		?x rdf:subject <ex:u-15000001> . " +
+		"		?x rdf:predicate <twitter:Has_Friend> . " +
+		"		?x rdf:object ?z . " +
+		"		FILTER( ?y <= \"2012-06-05T21:00:00\"^^xsd:dateTime ) " +
+		" } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+	              			  TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+	              			  TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-complex2-versioning" ) ;
+
+		//Queries that monitor various keywords
+		queryString = 
+		" PREFIX ex: <http://www.example.org/people#> " +
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" SELECT ?x ?y " +
+		" WHERE " +
+		" { " +
+		" 		?x tw:Has_Tweet ?y " +
+		"		?y tw:Tweet_Text ?z " +
+		"		FILTER regex(?z, \"bombing\", \"i\") " +
+		" } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+	              			  TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+	              			  TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-complex3-monitoring1" ) ;
+
+		queryString = 
+		" PREFIX ex: <http://www.example.org/people#> " +
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" SELECT ?x ?y " +
+		" WHERE " +
+		" { " +
+		" 		?x tw:Has_Tweet ?y " +
+		"		?y tw:Tweet_Text ?z " +
+		"		FILTER regex(?z, \".*(bombing|explosion).*america.*\", \"i\") " +
+		" } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+		             		  TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+		             		  TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-complex3-monitoring2" ) ;
+
+		queryString = 
+		" PREFIX ex: <http://www.example.org/people#> " +
+		" PREFIX twitter: <http://cs.utdallas.edu/semanticweb/StormRider/vocabs/twitter/0.1#> " +
+		" SELECT ?x ?y " +
+		" WHERE " +
+		" { " +
+		" 		?x tw:Has_Tweet ?y " +
+		"		?y tw:Tweet_Text ?z " +
+		"		?y tw:Tweet_Place \"Richardson,TX\"^^xsd:string " +
+		"		FILTER regex(?z, \".*(bombing|explosion).*america.*\", \"i\") " +
+		" } " ;
+		topology.submitQuery( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+		             		  TwitterConstants.MAX_REPORTS, TwitterConstants.INTERVAL, queryString, true,
+		             		  TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_CONFIG_FILE, "query-complex3-monitoring3" ) ;
+		
+		//Start the analyze topology
+		topology.submitAnalyzeTopology( TwitterConstants.IS_DISTRIBUTED, TwitterConstants.IS_LOCAL_MODE, TwitterConstants.NUM_OF_WORKERS, 
+										TwitterConstants.INTERVAL, true, TwitterConstants.HBASE_MODEL_CONFIG_FILE, TwitterConstants.HBASE_VIEW_CONFIG_FILE ) ;
 	}
 }
